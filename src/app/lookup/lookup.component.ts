866be855b2a9d75dee3a10d86bb326146b70abc9
Thibaud Huber <thibaud.huber@epitech.eu>
2019-03-24 19:51:26 -0400

    Calories Calculation with Example

--- src/app/lookup/lookup.component.ts
+++ src/app/lookup/lookup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CocktailService } from './../cocktail/cocktail.service';
 import { Cocktail } from '../cocktail/cocktail';
+import { NutritionalService } from '../nutritional/nutritional.service';
 @Component({
   selector: 'app-lookup',
   templateUrl: './lookup.component.html',
@@ -11,9 +12,10 @@ export class LookupComponent implements OnInit {
 
   public cocktail: Cocktail;
   public related: Array<{id: string, name: string, image: string, ref: number}> = [];
+  public calories = 0;
 
   constructor(private route: ActivatedRoute, private cocktailService: CocktailService,
-    private router: Router) { }
+    private router: Router, private nutritional: NutritionalService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -21,6 +23,15 @@ export class LookupComponent implements OnInit {
         this.cocktail = res;
         this.cocktailService.getRelatedCocktails(this.cocktail).then(related => {
           this.related = related;
+        });
+        this.nutritional.getNutrientsInfo(this.cocktail.ingredients).then(nutrients => {
+          console.log(nutrients);
+          if (nutrients['foods']) {
+            for (const nutrient of nutrients['foods']) {
+              this.calories += nutrient['nf_calories'];
+            }
+            console.log(this.calories);
+          }
         });
       });
     });
